529c987e8a1b636ab64c2eae948c55721a773851
Brian Bauska <brianbauska@gmail.com>
2023-03-13 19:09:58 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 var counterContainer = document.querySelector(".website-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
+// tooltips=begin
+const tooltip = document.getElementById("tooltip");
 
+const updateTooltipPosition = (event) => {
+  if (window.innerHeight > tooltip.offsetHeight + event.clientY) {
+    tooltip.style.top = `${event.clientY}px`;
+  }
+  
+  tooltip.style.left = `${event.clientX}px`;
+};
+// tooltips = end
 // Check if page_view entry is present
 if (visitCount) {
   visitCount = Number(visitCount) + 1;
